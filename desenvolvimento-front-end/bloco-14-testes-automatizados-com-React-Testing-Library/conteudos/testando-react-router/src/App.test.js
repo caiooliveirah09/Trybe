83b6846216001fc37bad0f591f3aef94aa6e8d15
caiooliveirah09 <leelros@hotmail.com>
2022-07-15 18:53:19 -0300

    front-end > exercise 14.3

--- desenvolvimento-front-end/bloco-14-testes-automatizados-com-React-Testing-Library/conteudos/testando-react-router/src/App.test.js
+++ desenvolvimento-front-end/bloco-14-testes-automatizados-com-React-Testing-Library/conteudos/testando-react-router/src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from './App';
+import renderWithRouter from './renderWithRouter'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('teste da aplicação toda', () => {
+  test('1-deve renderizar o component App', () => {
+    renderWithRouter(<App  />);
+
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  test('2-deve renderizar o componente Sobre', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', {
+      name: 'Sobre'
+    })
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitle = screen.getByRole('heading', {
+      name: 'Você está na página Sobre'
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('3-deve testar um caminho que não existe e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+    
+    const notFoundTitle = screen.getByRole('heading', {
+      name: 'Página não encontrada'
+    })
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+})
